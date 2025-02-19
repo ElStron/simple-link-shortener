72a56968f3355089a232a025295e3065ce2955ca
ElStron <luisstron45@gmail.com>
2025-02-18 22:20:37 -0600

    refactor: add Script component for clipboard functionality in Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -31,6 +31,7 @@ export const Home = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v7l3-3M9 7l3 3m0 11v-7l3 3m-6 0l3-3m6-5h1a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2h-1M6 9H5a2 2 0 0 0-2 2v2a2 2 0 0 0 2 2h1"/></svg>
                     </button>
                 </form>
+                <Script/>
                 <div id="loading" style="display:none" class="flex justify-center items-center gap-5">
                     <svg class="animate-spin h-10
                     w-10 text-cyan-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
@@ -43,3 +44,25 @@ export const Home = () => {
         </Layout>
     );
 };
+
+export const Script = () => html`
+    <script>
+       document.addEventListener('htmx:afterSwap', (event) => {
+            document.getElementById('form_url').reset();
+            const copyButton = document.getElementById('copy');
+            const resultUrl = document.getElementById('result_url');
+            const backupUrl = resultUrl.innerText || resultUrl.textContent;
+            copyButton.removeEventListener('click', copyFunction);
+
+            function copyFunction() {
+                navigator.clipboard.writeText(resultUrl.innerText || resultUrl.textContent);
+                resultUrl.innerText = 'Copied!';
+                setTimeout(() => {
+                    resultUrl.innerText = backupUrl;
+                }, 1000);
+            };
+
+            copyButton.addEventListener('click', copyFunction); 
+        });
+    </script>
+`;
